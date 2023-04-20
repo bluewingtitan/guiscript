0d186b71319c6839475108b9856d15d993d0c610
Dominik <38633608+bluewingtitan@users.noreply.github.com>
2023-04-21 00:21:15 +0200

    edit demo

--- demo/GuiScript.gd
+++ demo/GuiScript.gd
@@ -16,10 +16,10 @@ func _process(delta):
 	
 	# activate whatever sample you want to see.
 	# or activate all to see all.
-	drawWithTitle("Basics","drawBasics")
-	drawWithTitle("Draw Table", "drawTable")
-	drawWithTitle("Cookie Clicker", "drawCookieClicker")
-
+	#drawWithTitle("Basics","drawBasics")
+	#drawWithTitle("Draw Table", "drawDemoTable")
+	#drawWithTitle("Cookie Clicker", "drawCookieClicker")
+	drawDemoDebugMenu()
 
 # a simple helper to make any combination of function-components look good.
 func drawWithTitle(title, callName):
@@ -34,6 +34,67 @@ func drawWithTitle(title, callName):
 	
 	end_box()
 	
+
+# Reusable function to draw (bad looking) tables
+func drawTable(table_data):
+	begin_vbox()
+	
+	for row in table_data:
+		hseparator()
+		begin_hbox()
+		
+		for e in row:
+			vseparator()
+			label(str(e))
+			
+		vseparator()
+		end_box()
+		
+	hseparator()
+	end_box()
+
+# a simple MOCK of a debug menu
+func drawDemoDebugMenu():
+	begin_hbox()
+	
+	# SECTION 1: Debug Info
+	begin_vbox()
+	label("FPS: 55")
+	label("ENTITIES: 1337")
+	label("RAM: 755M")
+	label("PING: 14ms")
+	label("Timings: 6/3/1/9")
+	end_box()
+	
+	vseparator()
+	
+	# SECTION 2: Debug Tools
+	begin_vbox()
+	button("Reload Chunks")
+	button("Save Dump")
+	button("Pause Time")
+	button("Force Disconnect")
+	end_box()
+	
+	vseparator()
+	
+	# SECTION 3: Dev Cheats
+	begin_vbox()
+	button("Infinite Health")
+	button("Infinite Ammo")
+	button("Infinite Stamina")
+	button("Infinite Money")
+	button("Deactivate all Cheats")
+	end_box()
+	
+	end_box()
+	
+	hseparator()
+	
+	button("Close Debug Menu")
+	button("Close Game")
+	
+
 
 func drawBasics():
 	begin_hbox() # this creates a horizontal box
@@ -50,28 +111,16 @@ func drawBasics():
 	if button("Click Me!"):
 		print("Button was clicked!")
 
-var table_data = [
+var demo_table_data = [
 	[13, 37, 32, 40],
 	[50, 55, 56, 57],
 	[34, 34, 54, 62],
 	[42, 50, 13, 37],
 	]
-func drawTable():
-	begin_vbox()
-	
-	for row in table_data:
-		hseparator()
-		begin_hbox()
-		
-		for e in row:
-			vseparator()
-			label(str(e))
-			
-		vseparator()
-		end_box()
-		
-	hseparator()
-	end_box()
+
+func drawDemoTable():
+	drawTable(demo_table_data)
+
 	
 var clicks = 0.0
 var upgrade_cost = 5
